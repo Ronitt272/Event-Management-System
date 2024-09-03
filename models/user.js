f99d0d47734228f5bfbef47547451dff27e97895
Ronitt Mehra <mehraronitt@gmail.com>
2024-09-03 00:22:08 -0400

    Added email and password attributes to schema

--- models/user.js
+++ models/user.js
@@ -1,11 +1,16 @@
 let mongoose = require("mongoose")
 
 const userSchema = new mongoose.Schema({
+    email: String, 
+    Password: password,
     name: String,
     bio: String,
     friends: [{
         type: String
-      }]
+      }],
+    events: [{
+      type: String
+    }]
 })
 
 module.exports = new mongoose.model('user', userSchema);
